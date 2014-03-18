fde36e8f66e10c9ac257a6cd6e5513aae8d3e9b6
Matt Kaniaris <mkaniaris@gmail.com>
2014-03-18 14:08:11 -0700

    move

--- src/floobits_plugin/StartHandler.java
+++ src/floobits_plugin/StartHandler.java
@@ -1,11 +1,10 @@
-package floobitsplugin.handlers;
+package floobits_plugin;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import floobits_plugin.Activator;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
